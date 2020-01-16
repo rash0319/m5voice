d18e103e41c15a4660249f3315954f090169152d
rash0319 <shota@DESKTOP-F63NLM9.localdomain>
2020-01-16 15:10:31 +0900

    ボタンを押していた時間を図れるようにする

--- app.ino
+++ app.ino
@@ -5,10 +5,13 @@
 #include <M5Faces.h>
 #include <M5LoRa.h>
 #include <M5Stack.h>
+#include <stdio.h>
+#include <unistd.h>
 
-unsigned int guiBtnA_Cnt;
-unsigned int guiBtnB_Cnt;
-unsigned int guiBtnC_Cnt;
+unsigned int guiBtnA_Time;
+unsigned int guiBtnB_Time;
+unsigned int guiBtnC_Time;
+int hz = 1;
 
 void setup()
 {
@@ -16,20 +19,20 @@ void setup()
     M5.begin();
 
     // 初期化
-    guiBtnA_Cnt = 0;
-    guiBtnB_Cnt = 0;
-    guiBtnC_Cnt = 0;
+    guiBtnA_Time = 0;
+    guiBtnB_Time = 0;
+    guiBtnC_Time = 0;
     // 文字サイズを変更
     M5.Lcd.setTextSize(2);
     // Aボタン　カウンタ表示
     M5.Lcd.setCursor(50, 50);
-    M5.Lcd.printf("Button A : %d", guiBtnA_Cnt);
+    M5.Lcd.printf("Button A : %d", guiBtnA_Time);
     // Bボタン　カウンタ表示
     M5.Lcd.setCursor(50, 100);
-    M5.Lcd.printf("Button B : %d", guiBtnB_Cnt);
+    M5.Lcd.printf("Button B : %d", guiBtnB_Time);
     // Cボタン　カウンタ表示
     M5.Lcd.setCursor(50, 150);
-    M5.Lcd.printf("Button C : %d", guiBtnC_Cnt);
+    M5.Lcd.printf("Button C : %d", guiBtnC_Time);
 }
 
 void loop()
@@ -37,30 +40,31 @@ void loop()
 
     M5.update();
     // Aボタン
-    if (M5.BtnA.wasPressed())
+    // if (M5.BtnA.wasPressed())
+    if (M5.BtnA.isPressed())
     {
-        // カウンタ更新
-        guiBtnA_Cnt++;
+        guiBtnA_Time++;
         // カウンタ表示
         M5.Lcd.setCursor(50, 50);
-        M5.Lcd.printf("Button A : %d", guiBtnA_Cnt);
+        M5.Lcd.printf("Button A : %d", guiBtnA_Time);
     }
     // Bボタン
-    if (M5.BtnB.wasPressed())
+    if (M5.BtnB.isPressed())
     {
         // カウンタ更新
-        guiBtnB_Cnt++;
+        guiBtnB_Time++;
         // カウンタ表示
         M5.Lcd.setCursor(50, 100);
-        M5.Lcd.printf("Button B : %d", guiBtnB_Cnt);
+        M5.Lcd.printf("Button B : %d", guiBtnB_Time);
     }
     // Cボタン
-    if (M5.BtnC.wasPressed())
+    if (M5.BtnC.isPressed())
     {
         // カウンタ更新
-        guiBtnC_Cnt++;
+        guiBtnC_Time++;
         // カウンタ表示
         M5.Lcd.setCursor(50, 150);
-        M5.Lcd.printf("Button C : %d", guiBtnC_Cnt);
+        M5.Lcd.printf("Button C : %d", guiBtnC_Time);
     }
+    usleep(1000000 / hz);
 }
