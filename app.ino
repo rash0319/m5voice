e28711f147a8dce6148cfa5bb618d9db43988846
rash0319 <shota@DESKTOP-F63NLM9.localdomain>
2020-01-16 12:00:47 +0900

    hello world

---
+++ app.ino
@@ -0,0 +1,17 @@
+#include <Free_Fonts.h>
+#include <gitTagVersion.h>
+#include <LoRaWan.h>
+#include <M5Display.h>
+#include <M5Faces.h>
+#include <M5LoRa.h>
+#include <M5Stack.h>
+
+void setup()
+{
+    M5.begin();
+    M5.Lcd.println("Hello World.");
+}
+
+void loop()
+{
+}
